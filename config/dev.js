f5440bc86b6a99eb7b34928be8dd316a5ead9d35
Ujjval <ujjvalkumaria@gmail.com>
2019-06-07 01:32:11 -0700

    .gitignore Fixed

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientID: "82623729805-209pp2co4169us29d1b66pklcdibp3mq.apps.googleusercontent.com",
-    googleClientSecret: "FstbCj0CCiSS8ovZG00mkpml",
+    googleClientID: "659894947365-8cp9gnsogll18jhm5iaadmqkfj32rstf.apps.googleusercontent.com",
+    googleClientSecret: "24of_aYvUqJnaC6D1HdDrauQ",
     mongoURI: "mongodb://root:ujjval11@ds041157.mlab.com:41157/trippr",
     cookieKey: 'wakeupmycat',
     googleMapsKey: "AIzaSyAZn7wclMHXGAiymldyKJn1qAdDM84vk5A"
